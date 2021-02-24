2fbad9a016c6993498e5989eb4bd2fab9e76b59d
РАЙЫМБЕК <raymbek@MacBook-Pro-Raymbek.local>
2021-02-24 15:00:19 +0600

    app

---
+++ app/products.ts
@@ -0,0 +1,99 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Acer SB220Q bi ',
+    price: 93.99,
+    description: '21.5 inches Full HD (1920 x 1080) widescreen IPS display',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=sr_1_1?dchild=1&keywords=monitor&qid=1614109874&s=specialty-aps&sr=1-1',
+    rating: 4.7
+  },
+  {
+    id: 2,
+    name: 'LG 24M47VQ ',
+    price: 99.99,
+    description: '24-Inch LED-lit Monitor, Black',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91jzIGu5N-L._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/LG-24M47VQ-24-Inch-LED-lit-Monitor/dp/B00W95RR32/ref=sr_1_3?dchild=1&keywords=monitor&qid=1614111261&s=specialty-aps&sr=1-3',
+    rating: 4.7
+  },
+  {
+    id: 3,
+    name: 'ASUS VP279HE',
+    price: 149.99,
+    description: '27-inch Full HD (1920 x 1080) LED backlight display with IPS 178° wide viewing angle panel',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/716Pah1y6BL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/ASUS-VP279HE-Adaptive-Sync-Frameless-Mountable/dp/B08RH8VGG9/ref=sr_1_5?dchild=1&keywords=monitor&qid=1614111261&s=specialty-aps&sr=1-5',
+    rating: 4.4
+  },
+  {
+    id: 4,
+    name: 'LG 27UL500-W',
+    price: 348.51,
+    description: '27 inches UHD (3840 X 2160) IPS display',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81U4T6K5wQL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/LG-27UL500-W-27-Inch-Freesync-Technology/dp/B07PGL2WVS/ref=sr_1_17?dchild=1&keywords=monitor&qid=1614111261&s=specialty-aps&sr=1-17',
+    rating: 4.6
+  },
+  {
+    id: 5,
+    name: 'Dell Gaming S2721HGF',
+    price: 255.00,
+    description: '27 Inch Curved FHD 144Hz 1080p VA Ultra-Thin Bezel Monitor, Nvidia G-Sync and AMD FreeSync HDMI, DisplayPort, VESA Certified, Gray',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71t9SyPu-IL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Dell-S2721HGF-Ultra-Thin-DisplayPort-Certified/dp/B08DW4D6HP/ref=sr_1_24?dchild=1&keywords=monitor&qid=1614111261&s=specialty-aps&sr=1-24',
+    rating: 4.8
+  },
+  {
+    id: 6,
+    name: 'ASUS ProArt PA34VC',
+    price: 988.03,
+    description: '34.1” UWQHD (3440 x 1440) IPS 1900R curved 21: 9 display with 2 x Thunderbolt 3, DisplayPort 1.2 and 3 x HDMI 2.',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91F0uX46kGL._SL1500_.jpg',
+    link: 'https://www.amazon.com/ASUS-ProArt-PA34VC-Curved-Monitor/dp/B07R4RRRN7/ref=sr_1_25?dchild=1&keywords=monitor&qid=1614152530&s=specialty-aps&sr=1-25',
+    rating: 4.6
+  },
+  {
+    id: 7,
+    name: 'MSI Creator',
+    price: 799.99,
+    description: '32-inch WQHD (2560 x 1440 / 16:9) designed for creators & multi-taskers',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61cwQWUnZVL._AC_SL1200_.jpg',
+    link: 'https://www.amazon.com/MSI-Creator-PS321QR-165Hz-Response/dp/B08L77J9M8/ref=sr_1_38?dchild=1&keywords=monitor&qid=1614152641&s=specialty-aps&sr=1-38',
+    rating: 4.0
+  },
+  {
+    id: 8,
+    name: 'Dell UltraSharp U2720Q',
+    price: 629.00,
+    description: '27 Inch 4K UHD (3840 x 2160) LED Backlit LCD IPS USB-C Monitor (7GZ651)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/810ocpczsiL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Dell-UltraSharp-U2720Q-Backlit-Monitor/dp/B086H26XWV/ref=sr_1_62?dchild=1&keywords=monitor&qid=1614152800&s=specialty-aps&sr=1-62',
+    rating: 4.7
+  },
+  {
+    id: 9,
+    name: 'Samsung Business CH890 ',
+    price: 629.00,
+    description: '34-Inch 21: 9 WQHD 3440 x 1440 wide screen with picture-in-picture and split-screen mode allows for greater',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71qkzkC7bHL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Samsung-Business-3440x1440-Ultrawide-LC34H890WGNXGO/dp/B08623SZH9/ref=sr_1_63?dchild=1&keywords=monitor&qid=1614152800&s=specialty-aps&sr=1-63',
+    rating: 4.5
+  },
+  {
+    id: 10,
+    name: 'LG 34WL85C-B',
+    price: 596.99,
+    description: '34 Inch UltraWide Curved WQHD IPS Monitor with HDR 10,Black',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81B4SnLR1nL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/LG-34WL85C-B-UltraWide-Curved-Monitor/dp/B07N97DB99/ref=sr_1_73?dchild=1&keywords=monitor&qid=1614152962&s=specialty-aps&sr=1-73',
+    rating: 4.4
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
